945918a3f52287bba2b1897ff278768b1a5a947b
German Torales <germantorales73@gmail.com>
2022-11-20 14:46:13 -0300

    Config cors

--- src/app/app.js
+++ src/app/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const morgan = require("morgan");
+const cors = require("cors");
 const httpStatus = require("http-status");
 const { ValidationError } = require("express-validation");
 
@@ -12,11 +13,12 @@ const api = async (app) => {
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
   app.use(morgan("dev"));
+  app.use(cors({ origin: "*", optionsSuccessStatus: 200, methods: ["GET", "POST", "DELETE", "UPDATE", "PUT", "PATCH"] }));
 
   app.use("/api", router);
 
   app.use((req, res, next) => {
-    return res.status(httpStatus.NOT_FOUND).json({ code: httpStatus.NOT_FOUND, message: "Endpoint not found" });
+    return res.status(httpStatus.NOT_FOUND).json({ code: httpStatus.NOT_FOUND, message: `Endpoint ${req?.url} not found` });
   });
 
   app.use((err, req, res, next) => {
